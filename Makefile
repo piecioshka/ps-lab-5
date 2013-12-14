22647b754aab31fc00a9fe17b3ffe4816e1b821c
piecioshka <piecioshka@gmail.com>
2013-12-14 14:43:09 +0100

    after task 2

--- Makefile
+++ Makefile
@@ -6,6 +6,9 @@ SRC = source/
 zad1:
 	${CC} ${CFLAGS} ${SRC}$@/system.c -o ${TARGET}$@/system.o
 
+zad2:
+	${CC} ${CFLAGS} ${SRC}$@/main.c -o ${TARGET}$@/main.o
+
 clean:
 	find . -name "*.o" | xargs rm
 
---
+++ source/zad2/main.c
@@ -0,0 +1,41 @@
+/**
+ * Zadanie 2. Procesy - użycie funkcji fork()
+ * Napisz program, który tworzy jeden proces potomny. 
+ * Proces potomny wyświetla n razy zestaw małych liter alfabetu (czyli litery od a do z), 
+ * proces macierzysty wyświetla n razy zestaw wielkich liter alfabetu (czyli litery od A do Z.
+ * Wartość n jest podawana w wierszu wywołania programu. 
+ * Sprawdź, czy każde wykonanie programu będzie przebiegało tak samo? 
+ * Spróbuj uruchomić program na różnych komputerach, na przykład na oceanic. Wybierz duże n.
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+    int i, n, child;
+
+    if (argc < 2) {
+        fprintf(stderr, "Podac liczbę\n");
+        exit(1);
+    }
+
+    n = atoi(argv[1]);
+
+    // tworzymy nowy proces
+    switch (child = fork()) {
+        case -1: printf("error"); break;
+        case 0:
+            // potomny
+            for(i = 0; i < n; ++i) {
+                printf("a");
+            }
+            break;
+        default:
+            // macierzysty
+            for(i = 0; i < n; ++i) {
+                printf("A");
+            }
+    }
+
+    exit(0);
+}
