8d91e2696c0dedac0c07e039fed66ef77ad036ea
Piotr Kowalski <piecioshka@gmail.com>
2014-03-15 15:19:00 +0100

    Spaces

--- source/zad5/sigcatch.c
+++ source/zad5/sigcatch.c
@@ -1,11 +1,11 @@
 /**
- *
  * Zadanie 5. Obsługa sygnałów
  * a) Zapoznaj się z programem sigcatch.c.
  * Jakie jest zadanie funkcji signal() oraz pause()? 
  * Jak program zachowa się w przypadku otrzymana sygnału INT,
  * a jak w przypadku innego sygnału, np. QUIT, TERM?
  */
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <signal.h>
@@ -21,9 +21,11 @@ int main() {
     /* funkcja signal() pozwala na obsługę przechwyconego sygnału */
     signal(SIGINT, catch_ctlc);
     printf("Dzialam ...\n"); /* 1 */
+
     /* funkcja pause() zatrzymuje proces dopóki nie zostanie dostarczony sygnał, */
     /* program działa dalej gdy zakończy się działanie obsługi sygnału */
     pause();
+
     printf("Bye!\n"); /* 3 */
     return 0;
 }
